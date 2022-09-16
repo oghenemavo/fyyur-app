e0b196a1b54baf9f7002dca27d8f02f820244647
Mavi Onogomuho <mavi.onogomuho@titantrustbank.com>
2022-09-16 14:32:01 +0100

    setup env

--- config.py
+++ config.py
@@ -4,16 +4,16 @@ SECRET_KEY = os.urandom(32)
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Enable debug mode.
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
 # Connect to the database
 # DB Connection variables
-db_driver = 'postgresql'
-username = 'postgres'
-password = 'root'
-host_address = 'localhost'
-port = '5432'
-dbname = 'fyyur'
+db_driver = os.getenv('DB_DRIVER')
+username = os.getenv('USERNAME')
+password = os.getenv('PASSWORD')
+host_address = os.getenv('HOST_ADDRESS')
+port = os.getenv('PORT')
+dbname = os.getenv('DBNAME')
 
 # TODO IMPLEMENT DATABASE URL
 SQLALCHEMY_DATABASE_URI = db_driver + "://" + username + ":" + password + "@" + host_address + ":" + port + "/" + dbname
